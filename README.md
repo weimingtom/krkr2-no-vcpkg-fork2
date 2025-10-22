b627199927ba6bc1d5741adef421509923b88778
weimingtom <weimingtom@qq.com>
2025-10-22 15:47:59 +0800

    android adt project can be built and run

--- README.md
+++ README.md
@@ -28,6 +28,8 @@ https://github.com/2468785842/krkr2/tree/c08acbc134bbc26d62fc11b5c355efaed64d466
 * not sync Resources/ to project\android\assets and another assets
 * Remove TextStream.cpp's MY_USE_MINLIB work around code 
 * test android studio version
+* Start Android version with MainActivity instead of KR2Activity for requesting permissions
+* Sync SDL2 android version java and cpp code ???
 
 ## Recommended Development Environment for good audio music output 
 * Xubuntu 20.04 desktop amd64, in VMWare or VirtualBox  
---
+++ project/android/src/org/github/krkr2/MainActivity.java
@@ -0,0 +1,104 @@
+package org.github.krkr2;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.view.WindowManager;
+
+import org.cocos2dx.hellocpp.R;
+import org.libsdl.app.SDLAudioManager;
+import org.tvp.kirikiri2.KR2Activity;
+
+public class MainActivity extends KR2Activity {
+	  @Override 
+	  public void onCreate(Bundle savedInstanceState) {
+	        super.setEnableVirtualButton(false);
+	        super.onCreate(savedInstanceState);
+
+	        // Workaround in https://stackoverflow.com/questions/16283079/re-launch-of-activity-on-home-button-but-only-the-first-time/16447508
+	        if (!isTaskRoot()) {
+	            // Android launched another instance of the root activity into an existing task
+	            //  so just quietly finish and go away, dropping the user back into the activity
+	            //  at the top of the stack (ie: the last state of this task)
+	            // Don't need to finish it again since it's finished in super.onCreate .
+	            return;
+	        }
+
+	        // Make sure we're running on Pie or higher to change cutout mode
+	        // Enable rendering into the cutout area
+//	        WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+//	        lp.layoutInDisplayCutoutMode =
+//	            WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+//	        window.attributes = lp
+
+	        if (!checkStoragePermission()) {
+	            requestStoragePermission();
+	        }
+
+	        SDLAudioManager.nativeSetupJNI();
+	        SDLAudioManager.initialize();
+//	        SDLAudioManager.setContext(getContext());
+
+	    }
+
+	    @Override 
+	    public void onDestroy() {
+	        super.onDestroy();
+//	        SDLAudioManager.release(this);
+	    }
+
+	    private boolean checkStoragePermission() {
+	        // 检查 MANAGE_EXTERNAL_STORAGE 权限
+//	        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//	            return Environment.isExternalStorageManager()
+//	        }
+	    	
+	        return this.checkSelfPermission(
+	        		Manifest.permission.WRITE_EXTERNAL_STORAGE
+	        ) == PackageManager.PERMISSION_GRANTED;
+	    }
+
+	    // 请求用户授予 MANAGE_EXTERNAL_STORAGE 权限
+	    private boolean requestStoragePermission() {
+	        boolean r = false;
+
+//	        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+//	            registerForActivityResult(
+//	                ActivityResultContracts.RequestPermission()
+//	            ) { result -> r = result }
+//	                .launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//	            return r
+//	        }
+
+//	        boolean startForResult = registerForActivityResult(
+//	            ActivityResultContracts.StartActivityForResult()
+//	        ) { result ->
+//	            r = result.resultCode == 1 && checkStoragePermission()
+//	        }
+
+	        new AlertDialog.Builder(this)
+	            .setTitle(getString(R.string.request_storage_permission_title))
+	            .setMessage(getString(R.string.request_storage_permission))
+	            .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+//		                startForResult.launch(
+//		                        Intent(
+//		                            Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+//		                            Uri.fromParts("package", packageName, null)
+//		                        )
+//		                    )
+					}
+				})
+	            .setNegativeButton(getString(R.string.cancel), null)
+	            .show();
+	        return r;
+	    }
+}
